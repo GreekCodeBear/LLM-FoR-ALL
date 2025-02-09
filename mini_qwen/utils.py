0fb5a8a91bcb901543bc663a1aea774ad1865f43
Kylin <qcql@me.com>
2025-02-09 10:23:46 +0800

    finish sft&eval

--- mini_qwen/utils.py
+++ mini_qwen/utils.py
@@ -1,10 +1,27 @@
 import os
 from itertools import chain
 
-def find_files(dirs):
+def formatting_prompts_func(example):
+    output_texts = []
+    for i in range(len(example["conversations"])):
+        for item in example["conversations"][i]:
+            if item["from"] == "human":
+                human_text = item["value"]
+            elif item["from"] == "gpt":
+                gpt_text = item["value"]
+            else:
+                raise ValueError(f"Unknown sender: {item['from']}")
+        text = f"<|im_start|>user\n{human_text}<|im_end|>\n<|im_start|>assistant\n{gpt_text}<|im_end|>"
+        output_texts.append(text)
+    return output_texts
+
+def find_files(dirs,path="data/pt"):
+    """
+    遍历目录，查找所有文件
+    """
     files = []
     for dir in dirs:
-        base_path = os.path.join("data/pt", dir)
+        base_path = os.path.join(path, dir)
         for dirpath, _, filenames in os.walk(base_path):
             for filename in filenames:
                 if filename.endswith(".parquet"):
@@ -13,7 +30,9 @@ def find_files(dirs):
     return files
 
 def tokenize_dataset(examples,tokenizer,block_size=512):
-    """预处理预训练数据集，将文本分词并分块"""
+    """
+    预处理预训练数据集，将文本分词并分块
+    """
     eos_token = "<|im_end|>"
     text_examples = [text + eos_token for text in examples["text"]]  # 添加结束符
     tokenized_examples = tokenizer(text_examples, add_special_tokens=False)
@@ -30,3 +49,18 @@ def tokenize_dataset(examples,tokenizer,block_size=512):
         for k, t in concatenated_examples.items()
     }
     return result,total_length
+
+def print_trainable_parameters(model):
+    """
+    打印模型中可训练参数的数量。
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+    )
+    
